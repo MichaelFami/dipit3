e0ac4097bc049b00f6bed71a0c78189d1a6ce388
Michael Fami <michaelfami@gmail.com>
2018-06-14 16:51:01 -0500

    make it better

---
+++ resources/views/master.blade.php
@@ -0,0 +1,120 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="stylesheet" type="text/css" href="/css/app.css" />
+    <link rel="stylesheet" type="text/css" href="/css/slick.css" />
+    <link rel="stylesheet" type="text/css" href="/css/slick-theme.css" />
+    <title>@yield('title')</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-inverse navbar-expand-lg navbar-dark bg-danger">
+        <div class="container">
+            <div class="row w-100 no-gutters d-lg-flex d-flex flex-wrap justify-content-between">
+                <a class="navbar-brand col-lg-3 col-7 py-lg-3 order-lg-1 order-2 float-left d-block" href="/">
+                    <img id="dipItLogo" src="images/logo_orange.png" class="img-fluid">
+                </a>
+
+                <button class="navbar-toggler collapsed float-right order-3" type="button" data-toggle="collapse" data-target="#all-pages-176" aria-controls="all-pages-176" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div id="all-pages-176" class="navbar-collapse collapse float-right col-lg order-4">
+                    <ul class="navbar-nav justify-content-between ml-auto w-75">
+                        <li class="nav-item dropdown">
+                            <li class="nav-item active">
+                                <a class="nav-link" href="/courseCatalog">Home<span class="sr-only">(current)</span></a>
+                            </li>
+                            <span class="navbar-slash">/</span>
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Recipes</a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <a class="dropdown-item" href="#">Recipes</a>
+                                <a class="dropdown-item" href="#">Press</a>
+                                <a class="dropdown-item" href="/applicationForm">About Dip It</a>
+                            </div>
+                        </li>
+                        <span class="navbar-slash">/</span>
+                        <li class="nav-item">
+
+                            <a class="nav-link" href="/fullStack">Press</a>
+                        </li>
+                        <span class="navbar-slash">/</span>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">About Dip It</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Events</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Store Locator</a>
+                        </li>
+                        <span class="navbar-slash">/</span>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Contact Us</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </nav>
+    @yield('body')
+    <footer id="footer">
+        <div class="container-fluid py-4 bg-danger">
+            <div class="container">
+                <div class="row justify-content-between w-100">
+                    <div class="col-md-3 mb-md-0 mb-3">
+                        <p class="mbr-text">
+                            <br>
+                            <a class="text-white" href="#">ABOUT</a>
+                            <br>
+                            <a class="text-white" href="#">RECIPES</a>
+                            <br>
+                            <a class="text-white" href="#">PRESS</a>
+                            <hr>
+                            <a class="text-white" href="#">CONTACT</a>
+                        </p>
+                    </div>
+                    <div class="col-md-4 text-white text-center">
+                        <img id="" src="images/VTA-logo-white.png" alt="">
+                        <i class="fab fa-facebook-f fa-2x pr-4"></i>
+                        <i class="fab fa-linkedin-in fa-2x pr-4"></i>
+                        <i class="fab fa-twitter fa-2x pr-4"></i>
+                        <i class="fab fa-instagram fa-2x"></i>
+                    </div>
+                    <div class="col-md-3 text-white">
+                        <p>(956) 458-6513
+                            <br>
+                            <br> 106 international Bulevard, Suite 1-C
+                            <br> MISSION, TX 78572
+                        </p>
+                        <hr>
+
+                        <a class="text-white" href="#">WORK AT DIP IT</a>
+                    </div>
+                </div>
+                <div class="row text-center">
+                    <div class="col">
+                        <a class="text-white" href="#">Terms of Service</a>
+                        <span class="text-white">|</span>
+                        <a class="text-white" href="#">Privacy Policy</a>
+                        <br>
+                        <span class="text-white">&copy; 2018 DIP It. All Rights Reserved</span>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </footer>
+    <script src="/js/app.js"></script>
+    <!-- <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script> -->
+    <!-- <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script> -->
+    <script type="text/javascript" src="/js/slick/slick.min.js"></script>
+
+    @yield('bottomjs')
+</body>
+
+</html>
---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('landing');
+});
+
+Route::get('/fullStack', function (){
+    return view('fullStack');
+});
+
+Route::get('/applicationForm' , function (){
+    return view('applicationForm');
+});
+
+Route::get('/courseCatalog' , function (){
+    return view('courseCatalog');
+});
